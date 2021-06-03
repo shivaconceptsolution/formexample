09f8b614f26ec524d65e620fc9f33dd55598735e
shiva <50239824+shivaconceptsolution@users.noreply.github.com>
2021-06-03 10:06:19 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -73,3 +73,23 @@ def marksheet(request):
                   result = "FAIL"
         return render(request,"formexample/marksheet.html",{"key":result})
     return render(request,"formexample/marksheet.html")
+
+def feedback(request):
+    if request.method=="POST":
+      name = request.POST["txtname"]
+      gender = request.POST["gender"]
+      course = request.POST.getlist("course[]")
+      branch = request.POST.getlist("branch[]")
+      feed = request.POST["feed"]
+      c=""
+      for data in course:
+        c = c+data
+      b=""
+      for data in branch:
+         b = b + data + " "  
+      res = "Name is "+name + "Gender is "+gender + "Courses is "+c + "Branch is "+b + "feedback is "+feed
+
+      return render(request,"formexample/feedback.html",{"key":res})  
+
+
+    return render(request,"formexample/feedback.html")
